909f9120333ef2678f0a19f92b0eb56b8be332d0
kajugadaniels <kajugadaniels@gmail.com>
2024-05-08 15:13:37 +0200

    New updates for deployment

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-python3.12 -m venv venv
-venv\Scripts\activate
-python3.12 pip install -r requirement.txt
-python3.12 manage.py collectstatic
+echo "BUILD START"
+python3.12 -m pip install -r requirement.txt
+python3.12 manage.py collectstatic --noinput --clear
+echo "BUILD END"
